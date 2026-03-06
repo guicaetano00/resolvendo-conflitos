7c4c40d0c96b661f8997cbc150c70a96eb9e4cbf
PEDRO HENRIQUE DE ALMEIDA ELIAS <pedro.elias3@portalsesisp.org.br>
2026-03-06 11:34:24 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflito.ts
+++ conflito.ts
@@ -4,6 +4,7 @@ console.log(`alunoB inserindo a linha 2`);
 <<<<<<< HEAD
 let calculo: number = (75 + 75) /3; //imaginando um calculo qualquer
 =======
-let concatenar: string = `Git` + `Hub`;
+console.log(`Inserindo linha 7 pelo alunoB`);let concatenar: string = `Git` + `Hub`;
+
 console.log(`Concatenado ${concatenar}`);
 >>>>>>> 498d21dad6958029d7c4c5e992b4166324883918
